6febc1a8b10a2116003910199247bf07e06bd358
Бурячек Арсений Павлович <@castle.in>
2020-12-11 12:05:46 +0300

    feat: api Covid countries get

--- src/scripts/components/app/AppComponent.js
+++ src/scripts/components/app/AppComponent.js
@@ -1,9 +1,11 @@
+import DataService from '../../services/data.service'
 export default class AppComponent {
     constructor() {
-
+        this.dataservise = new DataService();
     }
 
     run() {
         console.log('App is running!');
+        this.allCityData = this.dataservise.getAllCityData();
     }
 }
--- src/scripts/services/data.service.js
+++ src/scripts/services/data.service.js
@@ -1,5 +1,27 @@
 export default class DataService {
     constructor() {
+        this.url = 'http://api.covid19api.com/';
+    }
 
+    async getData(dataRequest){
+        try {
+            let response = await fetch(`https://cors-anywhere.herokuapp.com/${this.url}${dataRequest}`);
+            let data = await response.json();
+            return data;
+        } catch (error) {
+            throw new Error('no data loaded');
+        }
+    }
+
+    async getAllCityData() {
+        const allCountries = 'summary'
+        let allCityData;
+        try {
+            allCityData = await this.getData(allCountries);
+            return allCityData;
+        }
+        catch (error) {
+            throw new Error('no data loaded');
+        }
     }
 }
